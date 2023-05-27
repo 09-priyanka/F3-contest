1baa906c17313bc9d58014764d7eb839793e127b
09-priyanka <125143662+09-priyanka@users.noreply.github.com>
2023-05-27 23:48:30 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,28 @@
+{
+    const key = 'userState'; 
+    const userStateString = localStorage.getItem(key);
+    if (!userStateString) {
+    
+        window.location.href = 'index.html';
+    }
+}
+
+
+const userStateString2 = localStorage.getItem('userState');
+const userState = JSON.parse(userStateString2);
+
+const name = userState.name;
+const email = userState.email;
+const password = userState.password;
+
+
+document.getElementById('name').textContent = name;
+document.getElementById('email').textContent = email;
+document.getElementById('password').textContent = password;
+
+const logout = document.getElementById("logout");
+logout.addEventListener("click", function(event){
+    event.preventDefault();
+    localStorage.removeItem("userState");
+    window.location.href = "index.html";
+})
